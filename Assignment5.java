42e5058ed1e4ffade2ffb261d9f9cf3f694e600d
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2023-08-16 00:59:30 +0530

    second Commit

---
+++ Assignment5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Assignment5 {
+
+   private static final Scanner Scanner=new Scanner(System.in);
+   public static void main(String[] args) {
+
+    final String CLEAR = "\033[H\033[2J";
+    final String COLOR_BLUE_BOLD = "\033[34;1m";
+    final String COLOR_RED_BOLD = "\033[31;1m";
+    final String COLOR_GREEN_BOLD = "\033[33;1m";
+    final String RESET = "\033[0m";
+
+    final String DASHBOARD = "\u1F4B5 Welcome to Smart Banking";
+    final String CREATE_NEWACCOUNT = "➕ Create New Account";
+    final String DEPOSITS = "DEPOSITS";
+    final String WITHDRAWS = "🖨 WITHDRAWS";
+    final String TRANSFERS = "🖨 TRANSFERS";
+    final String CHECK_ACCOUNT_BALANCE = "🖨 CHECK ACCOUNT BALANCE";
+    final String DELETE_ACCOUNT = "🖨 DELETE ACCOUNT";
+
+   }
+}
